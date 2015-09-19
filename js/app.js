3b0869fcbd700342482b988bfd161bc92db63b47
Kevin Hui <kh523@cornell.edu>
2015-09-19 17:56:13 -0400

    made button initially invisible until javascript loads. made audio file instantiate onload

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 
-	var audio = new Audio('/johncenashort.mp3');
+	var audio;
 
 	var imageSize = function() {
+		audio = new Audio('/johncenashort.mp3');
 		document.getElementById("button").style.width = Math.min(window.innerHeight, window.innerWidth);
+		document.getElementById("button").src = "img/button unpressed.png";
 	}
 
 	var clickDown = function() {
