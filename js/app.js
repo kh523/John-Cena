c26afd365e62663f0ef12fdecae0645570870c81
Kevin Hui <kh523@cornell.edu>
2015-09-19 16:37:01 -0400

    took out button pressed image because it was buggy on mobile. this makes the app smaller too

--- js/app.js
+++ js/app.js
@@ -2,23 +2,12 @@
 	var audio = new Audio('/johncenashort.mp3');
 
 	var imageSize = function() {
-		document.getElementById("buttonUp").style.width = Math.min(window.innerHeight, window.innerWidth);
-		document.getElementById("buttonUp").style.height = Math.min(window.innerHeight, window.innerWidth);
-		document.getElementById("buttonDown").style.width = Math.min(window.innerHeight, window.innerWidth);
-		document.getElementById("buttonDown").style.height = Math.min(window.innerHeight, window.innerWidth);
-
+		document.getElementById("button").style.width = Math.min(window.innerHeight, window.innerWidth);
 	}
 
 	var clickDown = function() {
 		audio.load();
 		audio.play();
-		document.getElementById("buttonUp").style.display = "none";
-		document.getElementById("buttonDown").style.display = "block";
-	}
-
-	var clickUp = function() {
-		document.getElementById("buttonUp").style.display = "block";
-		document.getElementById("buttonDown").style.display = "none";
 	}
 
 	
