bba1761e98dfdbb8623dc114ee847052609f7ffd
Kevin Hui <kh523@cornell.edu>
2015-09-25 13:19:57 -0400

    added global counter

--- js/app.js
+++ js/app.js
@@ -1,16 +1,37 @@
 
-	var audio;
+var audio;
+var totalclicks;
+var myDataRef = new Firebase('https://john-cenaaaa.firebaseio.com/');
 
-	var imageSize = function() {
-		audio = new Audio('/johncenashort.mp3');
-		document.getElementById("button").style.width = Math.min(window.innerHeight, window.innerWidth);
-		document.getElementById("button").src = "img/button unpressed.png";
-	}
+var imageSize = function() {
+	audio = new Audio('/johncenashort.mp3');
+	document.getElementById("button").style.width = Math.min(window.innerHeight, window.innerWidth);
+	document.getElementById("button").src = "img/button unpressed.png";
 
-	var clickDown = function() {
-		audio.pause();
-		audio.currentTime = 0;
-		audio.play();
-	}
+	myDataRef.once("value", function(data) {
+		console.log(data.exportVal().totalclicks);
+		totalclicks = data.exportVal().totalclicks;
+		updateCounter();
+	});
+    
+}
 
-	
+var clickDown = function() {
+	audio.pause();
+	audio.currentTime = 0;
+	audio.play();
+	totalclicks++;
+	myDataRef.set({totalclicks: totalclicks});
+	updateCounter();
+}
+
+var updateCounter = function() {
+	document.getElementById("totalclicks").innerHTML = "Total Clicks: " + totalclicks;
+}
+
+myDataRef.on('child_changed', function(childSnapshot, prevChildKey) {
+	totalclicks = childSnapshot.val();
+	updateCounter;
+});
+
+
